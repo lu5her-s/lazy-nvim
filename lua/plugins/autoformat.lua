e42b36510b0ed29617006a207ca8a1c80f7a9d5a
lu5her-s <lu5her0sav1or@gmail.com>
2023-02-20 15:32:13 +0700

    add autopep8 and autoformat

---
+++ lua/plugins/autoformat.lua
@@ -0,0 +1,10 @@
+return {
+  "vim-autoformat/vim-autoformat",
+  keys = {
+    {
+      "<leader>al",
+      "AutoformatLine <CR>",
+      desc = "Auto Format Line",
+    },
+  },
+}
---
+++ lua/plugins/autopep8.lua
@@ -0,0 +1,10 @@
+return {
+  "tell-k/vim-autopep8",
+  keys = {
+    {
+      "<leader>ap",
+      "Autopep8()",
+      desc = "Autopep8",
+    },
+  },
+}
