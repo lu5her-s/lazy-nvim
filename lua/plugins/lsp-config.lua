7fd110cbdcd82b2b59ef1a006d10f528080f397f
lu5her-s <lu5her0sav1or@gmail.com>
2023-04-25 19:07:14 +0700

    not use pyright

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -19,7 +19,7 @@ return {
       ---@type lspconfig.options
       servers = {
         -- pyright will be automatically installed with mason and loaded with lspconfig
-        pyright = {},
+        -- pyright = {},
       },
     },
   },
