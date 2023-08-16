5002cd04202d5beabb5fd4eb5c8a7fd33695e690
lu5her-s <lu5her0sav1or@gmail.com>
2023-08-16 14:16:47 +0700

    add codeGpt

---
+++ lua/plugins/codeGpt.lua
@@ -0,0 +1,17 @@
+return {
+  "dpayne/CodeGPT.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("codegpt.config")
+  end,
+  keys = {
+    {
+      "<C-c>",
+      ":Chat",
+      desc = "CodeGpt",
+    },
+  },
+}
