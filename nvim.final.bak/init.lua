0ba2b23a7bafc49d82eb592c1ee10cd272458d13
lu5her-s <lu5her0sav1or@gmail.com>
2024-03-08 21:09:08 +0700

    edit for update LazyVim

--- nvim.final.bak/init.lua
+++
@@ -1,6 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
--- example using a list of specs with the default options
--- vim.g.mapleader = "\\"
--- make sure to set `mapleader` before lazy so your mappings are correct
