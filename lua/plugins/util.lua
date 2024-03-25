d034a701bbd733797195e8d6682325fc509b532d
lu5her-s <lu5her0sav1or@gmail.com>
2024-03-25 10:12:49 +0700

    Edit ui

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -6,20 +6,20 @@ return {
     cmd = "DiffviewOpen",
   },
 
-  {
-    "vhyrro/luarocks.nvim",
-    config = function()
-      require("luarocks").setup({})
-    end,
-  },
-  {
-    "rest-nvim/rest.nvim",
-    ft = "http",
-    dependencies = { "luarocks.nvim" },
-    config = function()
-      require("rest-nvim").setup()
-    end,
-  },
+  -- {
+  --   "vhyrro/luarocks.nvim",
+  --   config = function()
+  --     require("luarocks").setup({})
+  --   end,
+  -- },
+  -- {
+  --   "rest-nvim/rest.nvim",
+  --   ft = "http",
+  --   dependencies = { "luarocks.nvim" },
+  --   config = function()
+  --     require("rest-nvim").setup()
+  --   end,
+  -- },
 
   -- emmet
   {
