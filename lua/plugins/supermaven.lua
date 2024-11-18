f48c824a5435aa836e0e4e10a398c3bf783611a6
lu5her-s <lu5her0sav1or@gmail.com>
2024-11-18 18:10:07 +0700

    edit use both codeium and supermaven

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -2,7 +2,16 @@ return {
   {
     "supermaven-inc/supermaven-nvim",
     config = function()
-      require("supermaven-nvim").setup({})
+      require("supermaven-nvim").setup({
+        keymaps = {
+          accept_suggestion = "<C-y>",
+        },
+        color = {
+          -- want pastel yellow
+          suggestion_color = "#FFFAA0",
+          cterm = 244,
+        },
+      })
     end,
   },
 }
